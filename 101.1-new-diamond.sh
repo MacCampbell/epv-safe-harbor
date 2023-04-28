08e15c1529868dc0955673f9cb9a9ef876f9e56e
maccampbell <campbellmatthewa@gmail.com>
2023-04-28 11:27:46 -0800

    Corrected meta

--- 101.1-new-diamond.sh
+++ 101.1-new-diamond.sh
@@ -1,6 +1,6 @@
 diamond blastx -d  reference/diamond/seqs -q data/raw/Control-1_R1_001.fastq.gz -o outputs/101/Control-1-diamond
 diamond blastx -d  reference/diamond/seqs -q data/raw/Control-2_R1_001.fastq.gz -o outputs/101/Control-2-diamond
-diamond blastx -d  reference/diamond/seqs -q data/raw/Control-2_R1_001.fastq.gz -o outputs/101/Control-3-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/Control-3_R1_001.fastq.gz -o outputs/101/Control-3-diamond
 diamond blastx -d  reference/diamond/seqs -q data/raw/NUP-1_R1_001.fastq.gz -o outputs/101/NUP-1-diamond
 diamond blastx -d  reference/diamond/seqs -q data/raw/NUP-2_R1_001.fastq.gz -o outputs/101/NUP-2-diamond
 diamond blastx -d  reference/diamond/seqs -q data/raw/NUP-3_R1_001.fastq.gz -o outputs/101/NUP-3-diamond

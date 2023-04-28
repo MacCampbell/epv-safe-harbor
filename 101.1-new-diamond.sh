14f099cd3d7f4199b23e083383aedc0aa7f7a170
maccampbell <campbellmatthewa@gmail.com>
2023-04-28 10:35:03 -0800

    Setting up diamond

---
+++ 101.1-new-diamond.sh
@@ -0,0 +1,10 @@
+Command
+diamond blastx -d  reference/diamond/seqs -q data/raw/Control-1_R1_001.fastq.gz -o outputs/101/Control-1-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/Control-2_R1_001.fastq.gz -o outputs/101/Control-2-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/Control-2_R1_001.fastq.gz -o outputs/101/Control-3-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/NUP-1_R1_001.fastq.gz -o outputs/101/NUP-1-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/NUP-2_R1_001.fastq.gz -o outputs/101/NUP-2-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/NUP-3_R1_001.fastq.gz -o outputs/101/NUP-3-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/PVT-1_R1_001.fastq.gz -o outputs/101/PVT-1-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/PVT-2_R1_001.fastq.gz -o outputs/101/PVT-2-diamond
+diamond blastx -d  reference/diamond/seqs -q data/raw/PVT-3_R1_001.fastq.gz -o outputs/101/PVT-3-diamond
